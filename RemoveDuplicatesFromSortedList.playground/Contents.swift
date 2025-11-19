f3f4e981411316bf98659a93d3c7dcbb333d9bbe
Miguel Torres <99153283+MrPicudo@users.noreply.github.com>
2025-11-19 15:41:26 -0600

    Remove duplicates from sorted list
    - Analizamos el recorrido de una lista simplemente enlazada, ya que si avanzamos hasta el final de la lista, sin almacenar el head, no es posible regresar.
    - Creamos todos los nodos como una lista opcional para poder acceder a sus valores de manera correcta.
    - Iteramos sobre la lista original que nos dan, a través de currentNode y modificamos la nueva lista que retornamos, a través de newTail.

---
+++ RemoveDuplicatesFromSortedList.playground/Contents.swift
@@ -0,0 +1,62 @@
+/* ELIMINAR DUPLICADOS DE UNA LISTA SIMPLEMENTE ENLAZADA ORDENADA.
+Dada la cabeza de una lista enlazada ordenada, elimina todos los nodos que sean duplicados de modo que cada valor aparezca una sola vez, y devuelve la lista resultante igualmente ordenada. Por ejemplo, de [1,1,2] se obtiene [1,2] y de [1,1,2,3,3] se obtiene [1,2,3]. La lista puede tener entre 0 y 300 nodos, los valores de los nodos están entre -100 y 100, y la lista ya viene ordenada de forma ascendente.
+
+Given the head of a sorted linked list, remove all duplicate nodes so that each value appears only once, and return the resulting sorted list. For example, [1,1,2] becomes [1,2] and [1,1,2,3,3] becomes [1,2,3]. The list length is between 0 and 300 nodes, node values range from -100 to 100, and the input list is guaranteed to be sorted in ascending order.
+ */
+
+/// Clase dada por Leetcode
+public class ListNode: CustomStringConvertible {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    
+    public var description: String {
+        guard let next else { return "\(val)" }
+        return "\(val) -> " + String(describing: next) + " "
+    }
+}
+
+/// Solución original
+func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+    // Consideramos el caso en que la lista sea nula
+    if head == nil { return nil }
+    // Creamos los nodos head y tail para nuestra solución
+    var newHead: ListNode? = ListNode(head!.val)
+    var newTail: ListNode? = newHead
+    // Nombramos al nodo sobre el que vamos a iterar, currentNode
+    var currentNode: ListNode? = head!.next
+    
+    // Iteramos hasta que el currentNode sea distinto de nil
+    while currentNode != nil {
+        // Solamente cuando el valor del nodo sea distinto al del tail, agregamos un nuevo nodo en next.
+        if newTail!.val != currentNode!.val {
+            newTail!.next = ListNode(currentNode!.val)
+            newTail = newTail!.next
+        }
+        currentNode = currentNode!.next
+    }
+
+    return newHead
+}
+
+
+///Pruebas
+deleteDuplicates(
+    ListNode(1,
+             ListNode(3,
+                      ListNode(4,
+                               ListNode(4,
+                                        ListNode(23,
+                                                 ListNode(23,
+                                                          ListNode(30)
+                                                         )
+                                                )
+                                       )
+                              )
+                     )
+            )
+)
+
+deleteDuplicates(nil)
